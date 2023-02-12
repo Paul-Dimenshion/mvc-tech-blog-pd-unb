7975db5fe5a31573360335f55f2c7e3ffeae9cca
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-02-11 22:25:26 -0400

    router instance

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+/* The express.Router class creates modular, mountable route handlers
+A Router instance is a complete middleware and routing system;
+for this reason, it is often referred to as a “mini-app” */
+const router = require('express').Router();
+
+// set up routes here
+const apiRoutes = require("./api")
+const homeRoutes = require('./homeRoutes');
+
+// the "Home Routes" do not need the "/api" prefix
+router.use('/', homeRoutes);
+
+// the "API Routes" need the prefix of "/api" to work
+router.use('/api', apiRoutes);
+
+module.exports = router;
